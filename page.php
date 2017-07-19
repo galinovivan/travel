3d9e474d2212e825b553bdfd8acbba0e0ac487b2
galinovivan <9225913@gmail.com>
2017-07-19 14:51:46 +0300

    add page template

--- page.php
+++ page.php
@@ -2,10 +2,8 @@
 <?php while(have_posts()): the_post(); ?>
     <div class="container page_content">
         <h1 class="page_title"><?=get_the_title();?></h1>
-        <div class="content_container">
-    <?php the_content(); ?>
-            <?php if(get_theme_mod('avvocato_header_phone')) { ?><li><i class="fa fa-phone"></i><a href="<?php echo esc_url( 'phone:'. esc_attr(get_theme_mod('avvocato_header_phone'))); ?>"><?php echo esc_attr(get_theme_mod('avvocato_header_phone')); ?></a></li><?php } ?>
-            <?php if(get_theme_mod('avvocato_header_email')) { ?><li><i class="fa fa-envelope"></i><a href="<?php echo esc_url( 'mailto:' . antispambot(sanitize_email(get_theme_mod('avvocato_header_email')))); ?>"><?php echo antispambot(sanitize_email(get_theme_mod('avvocato_header_email'))); ?></a></li><?php } ?>
+        <div class="content">
+            <?php the_content(); ?>
         </div>
     </div>
 <?php endwhile; ?>
